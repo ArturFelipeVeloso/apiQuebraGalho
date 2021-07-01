ca8bd5e364fa621f4fcbec87922e8178779f44be
Artur Felipe Veloso <arturfdasveloso@gmail.com>
2021-07-01 19:18:36 -0300

    Primeiro

---
+++ apps/api/viewsets.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets
+from .serializers import *
+from .models import Categoria, Vendedor, ItensVenda, Cliente, Produto, Venda
+
+class VendedorViewSet(viewsets.ModelViewSet):
+    queryset = Vendedor.objects.all()
+    serializer_class = VendedorSerializer
+    
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    
+class ItensVendaViewSet(viewsets.ModelViewSet):
+    queryset = ItensVenda.objects.all()
+    serializer_class = ItensVendaSerializer
+    
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+    
+class VendaViewSet(viewsets.ModelViewSet):
+    queryset = Venda.objects.all()
+    serializer_class = VendaSerializer
